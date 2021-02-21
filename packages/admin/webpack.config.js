1ea5fa4ab41a21bf08a00a82b160063217e9a832
Robin <contact@piedweb.com>
2021-02-21 13:10:28 +0100

    update admin assets

--- packages/admin/webpack.config.js
+++ packages/admin/webpack.config.js
@@ -1,26 +1,26 @@
-var Encore = require('@symfony/webpack-encore');
+var Encore = require("@symfony/webpack-encore");
 
-Encore.setOutputPath('./src/Resources/public/')
-  .setPublicPath('./')
-  .setManifestKeyPrefix('bundles/pushwordadmin')
+Encore.setOutputPath("./src/Resources/public/")
+    .setPublicPath("./")
+    .setManifestKeyPrefix("bundles/pushwordadmin")
 
-  .cleanupOutputBeforeBuild()
-  .enableSassLoader()
-  .enableSourceMaps(false)
-  .enableVersioning(false)
-  .disableSingleRuntimeChunk()
-  .copyFiles({
-    from: './node_modules/ace-builds/src-min-noconflict/',
-    // relative to the output dir
-    to: 'ace/[name].[ext]',
-    // only copy files matching this pattern
-    pattern: /\.js$/,
-  })
-  .copyFiles({
-    from: './src/Resources/assets/',
-    to: '[name].[ext]',
-    pattern: /logo.svg$/,
-  })
-  .addEntry('admin', './src/Resources/assets/admin.js'); // {{ asset('bundles/pushwordadmin/admin.js') }}
+    .cleanupOutputBeforeBuild()
+    .enableSassLoader()
+    .enableSourceMaps(false)
+    .enableVersioning(false)
+    .disableSingleRuntimeChunk()
+    .copyFiles({
+        from: "./node_modules/ace-builds/src-min-noconflict/",
+        // relative to the output dir
+        to: "ace/[name].[ext]",
+        // only copy files matching this pattern
+        pattern: /\.js$/,
+    })
+    .copyFiles({
+        from: "./src/Resources/assets/",
+        to: "[name].[ext]",
+        pattern: /logo.svg$/,
+    })
+    .addEntry("admin.min", "./src/Resources/assets/admin.js"); // {{ asset('bundles/pushwordadmin/admin.js') }}
 
 module.exports = Encore.getWebpackConfig();
