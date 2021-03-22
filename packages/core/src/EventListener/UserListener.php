e52681e5a4f19003f47e62854781c615c49be66b
Robin <contact@piedweb.com>
2021-03-22 18:58:12 +0100

    fix create user

--- packages/core/src/EventListener/UserListener.php
+++ packages/core/src/EventListener/UserListener.php
@@ -2,7 +2,9 @@
 
 namespace Pushword\Core\EventListener;
 
+use Doctrine\ORM\Event\LifecycleEventArgs;
 use Doctrine\ORM\Event\PreUpdateEventArgs;
+use Doctrine\ORM\Mapping\PreUpdate;
 use Pushword\Core\Entity\UserInterface;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
@@ -18,11 +20,16 @@ class UserListener
     /**
      * Set Password on database update if PlainPassword is set.
      */
-    public function preUpdate(UserInterface $user, PreUpdateEventArgs $event)
+    public function preUpdate(UserInterface $user, PreUpdateEventArgs $event = null)
     {
         if (\strlen($user->getPlainPassword()) > 0) {
             $user->setPassword($this->passwordEncoder->encodePassword($user, $user->getPlainPassword()));
             $user->eraseCredentials();
         }
+    }
+
+    public function prePersist(UserInterface $user, LifecycleEventArgs $event)
+    {
+        return $this->preUpdate($user);
     }
 }
