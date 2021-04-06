304b54eb1c745940976b1cb456f1d6eff35fe7ca
Robin <contact@piedweb.com>
2021-04-06 15:33:56 +0200

    update navbar

--- packages/core/tailwind.config.js
+++ packages/core/tailwind.config.js
@@ -1,30 +1,32 @@
 module.exports = {
-  purge: {}, // directly in webpack
-  theme: {
-    extend: {
-      typography: {
-        DEFAULT: {
-          css: {
-            color: '#333',
-            a: {
-              color: 'var(--primary)',
-              '&:hover': {
-                opacity: '.75',
-              },
-            },
-          },
+    //mode: "jit",
+    purge: {}, // directly in webpack
+    theme: {
+        minHeight: {
+            0: "0",
+            "vw-3/4": "75vh",
+            full: "100%",
         },
-      },
-      colors: {
-        primary: 'var(--primary)',
-        secondary: 'var(--secondary)',
-      },
+        extend: {
+            typography: {
+                DEFAULT: {
+                    css: {
+                        color: "#333",
+                        a: {
+                            color: "var(--primary)",
+                            "&:hover": {
+                                opacity: ".75",
+                            },
+                        },
+                    },
+                },
+            },
+            colors: {
+                primary: "var(--primary)",
+                secondary: "var(--secondary)",
+            },
+        },
     },
-  },
-  variants: {},
-  plugins: [
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/forms'),
-  ],
+    variants: {},
+    plugins: [require("@tailwindcss/typography"), require("@tailwindcss/aspect-ratio"), require("@tailwindcss/forms")],
 };
