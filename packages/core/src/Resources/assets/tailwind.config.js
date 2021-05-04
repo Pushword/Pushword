4c5f0edbc56ab761397b710e1697cbfd978a5830
RobinDev <contact@piedweb.com>
2021-05-04 17:52:08 +0200

    minor ui imprvmnt

--- packages/core/src/Resources/assets/tailwind.config.js
+++ packages/core/src/Resources/assets/tailwind.config.js
@@ -1,4 +1,5 @@
 const plugin = require("tailwindcss/plugin");
+const colors = require("tailwindcss/colors");
 
 module.exports = {
     mode: "jit",
@@ -13,6 +14,34 @@ module.exports = {
             "screen-1/2": "50vh",
             screen: "100vh",
             full: "100%",
+        },
+        colors: {
+            transparent: "transparent",
+            current: "currentColor",
+            black: "#000",
+            white: "#fff",
+            bluegray: colors.blueGray,
+            coolgray: colors.coolGray,
+            gray: colors.gray,
+            truegray: colors.trueGray,
+            warmgray: colors.warmGray,
+            red: colors.red,
+            orange: colors.orange,
+            amber: colors.amber,
+            yellow: colors.yellow,
+            lime: colors.lime,
+            green: colors.green,
+            emerald: colors.emerald,
+            teal: colors.teal,
+            cyan: colors.cyan,
+            lightblue: colors.lightBlue,
+            blue: colors.blue,
+            indigo: colors.indigo,
+            violet: colors.violet,
+            purple: colors.purple,
+            fuchsia: colors.fuchsia,
+            pink: colors.pink,
+            rose: colors.rose,
         },
         extend: {
             typography: {
