77c392bf3bfdd80dcf5fb91afb5bf35e0780b613
RobinDev <contact@piedweb.com>
2021-05-05 10:43:32 +0200

    fix cs

--- packages/admin/src/FormField/PageParentPageField.php
+++ packages/admin/src/FormField/PageParentPageField.php
@@ -2,8 +2,6 @@
 
 namespace Pushword\Admin\FormField;
 
-use Doctrine\ORM\QueryBuilder;
-use Pushword\Core\Entity\PageInterface;
 use Pushword\Core\Repository\PageRepository;
 use Sonata\AdminBundle\Form\FormMapper;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
--- packages/core/src/Twig/EmailTwigTrait.php
+++ packages/core/src/Twig/EmailTwigTrait.php
@@ -3,10 +3,8 @@
 namespace Pushword\Core\Twig;
 
 use Pushword\Core\Component\App\AppConfig;
-use Twig\Environment as Twig;
 
 trait EmailTwigTrait
 {
     abstract public function getApp(): AppConfig;
-
 }
