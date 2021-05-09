2b3a042518e050992bd960a3f2cfd127883d5c7a
RobinDev <contact@piedweb.com>
2021-05-09 10:07:03 +0200

    add image/jpeg

--- packages/admin/src/Resources/assets/admin.filterImageFormField.js
+++ packages/admin/src/Resources/assets/admin.filterImageFormField.js
@@ -2,7 +2,7 @@ export function filterImageFormField() {
     const addToHref = function (element) {
         element.href =
             element.href +
-            "&[mimeType][value][]=image/gif&filter[mimeType][value][]=image/png&filter[mimeType][value][]=image/jpg";
+            "&[mimeType][value][]=image/gif&filter[mimeType][value][]=image/png&filter[mimeType][value][]=image/jpg&filter[mimeType][value][]=image/jpeg";
     };
 
     let mainImage = document.querySelector('span[id$="_mainImage"] a');
