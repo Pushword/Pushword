b97c77c2e24a871caf47ac395e161572d4c89ae9
RobinDev <contact@piedweb.com>
2022-01-20 11:50:19 +0100

    update pushword/js-helper version

--- packages/js-helper/src/tailwind.config.js
+++ packages/js-helper/src/tailwind.config.js
@@ -24,6 +24,7 @@ module.exports = {
     },
     variants: {},
     plugins: [
+        require("tailwindcss-multi-column")(),
         require("@tailwindcss/typography"),
         require("@tailwindcss/aspect-ratio"),
         require("@tailwindcss/forms"),
