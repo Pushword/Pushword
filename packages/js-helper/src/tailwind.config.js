0167d3efbd451d1d6df0d2d4526fe3b4ae202444
RobinDev <contact@piedweb.com>
2022-04-30 09:09:48 +0200

    [Frontend] Add line-clamp tw plugin

--- packages/js-helper/src/tailwind.config.js
+++ packages/js-helper/src/tailwind.config.js
@@ -1,33 +1,34 @@
-const plugin = require("tailwindcss/plugin");
-const pushwordHelper = require("@pushword/js-helper/src/tailwind.helpers.js");
+const plugin = require('tailwindcss/plugin');
+const pushwordHelper = require('@pushword/js-helper/src/tailwind.helpers.js');
 
 module.exports = {
-    mode: "jit",
+    mode: 'jit',
     theme: {
         minHeight: {
-            0: "0",
-            "screen-1/4": "25vh",
-            "screen-3/4": "75vh",
-            "screen-1/3": "33vh",
-            "screen-2/3": "66vh",
-            "screen-1/2": "50vh",
-            screen: "100vh",
-            full: "100%",
+            0: '0',
+            'screen-1/4': '25vh',
+            'screen-3/4': '75vh',
+            'screen-1/3': '33vh',
+            'screen-2/3': '66vh',
+            'screen-1/2': '50vh',
+            screen: '100vh',
+            full: '100%',
         },
         extend: {
             typography: pushwordHelper.extendTailwindTypography(),
             colors: {
-                primary: "var(--primary)",
-                secondary: "var(--secondary)",
+                primary: 'var(--primary)',
+                secondary: 'var(--secondary)',
             },
         },
     },
     variants: {},
     plugins: [
-        require("tailwindcss-multi-column")(),
-        require("@tailwindcss/typography"),
-        require("@tailwindcss/aspect-ratio"),
-        require("@tailwindcss/forms"),
+        require('tailwindcss-multi-column')(),
+        require('@tailwindcss/typography'),
+        require('@tailwindcss/aspect-ratio'),
+        require('@tailwindcss/line-clamp'),
+        require('@tailwindcss/forms'),
         plugin(pushwordHelper.twFirstLetterPlugin),
         plugin(pushwordHelper.twFirstChildPlugin),
         plugin(pushwordHelper.twBleedPlugin),
