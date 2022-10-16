fc82880eafe6bf1694db267d02aab5a167ae7091
RobinDev <contact@piedweb.com>
2022-10-16 17:01:14 +0200

    ...

--- packages/core/src/Utils/Entity.php
+++ packages/core/src/Utils/Entity.php
@@ -7,14 +7,29 @@ use Doctrine\ORM\Mapping\Column;
 class Entity
 {
     /**
+     * @param \ReflectionAttribute[] $attributes
+     */
+    private static function containAttribute(array $attributes, string $searchedName): bool // @phpstan-ignore-line
+    {
+        foreach ($attributes as $attribute) {
+            if ($attribute->getName() === $searchedName) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
      * @return list<string>
      */
     public static function getProperties(object $object): array
     {
         $reflectionClass = new \ReflectionClass($object::class);
         $properties = array_filter($reflectionClass->getProperties(), function (\ReflectionProperty $property) {
+            $attributes = $property->getAttributes();
             if (str_contains((string) $property->getDocComment(), '@ORM\Column')
-                || str_contains(implode(',', $property->getAttributes()), Column::class)) {
+                || self::containAttribute($property->getAttributes(),  Column::class)) {
                 return true;
             }
         });
