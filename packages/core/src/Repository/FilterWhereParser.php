3b1214ac55c574084775d83aa74a96c8e73f33e6
RobinDev <contact@piedweb.com>
2023-11-19 13:12:25 +0100

    separate class for manage pages list where filter in repo

--- packages/core/src/Repository/FilterWhereParser.php
+++ packages/core/src/Repository/FilterWhereParser.php
@@ -19,7 +19,7 @@ class FilterWhereParser
      * @param array<mixed> $where
      */
     public function __construct(
-        private QueryBuilder $queryBuilder,
+        private readonly QueryBuilder $queryBuilder,
         private array $where
     ) {
     }
