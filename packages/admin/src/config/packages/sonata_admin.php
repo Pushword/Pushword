56b4e87644d5ae5df11f9c6e53087f37dc0f003f
RobinDev <contact@piedweb.com>
2023-11-21 11:50:07 +0100

    fix icon

--- packages/admin/src/config/packages/sonata_admin.php
+++ packages/admin/src/config/packages/sonata_admin.php
@@ -24,7 +24,7 @@ return [
                     'on_top' => true,
                     'label' => 'admin.label.media', // 'admin.label.params',
                     'translation_domain' => 'messages',
-                    'icon' => '<i class="fa fa-photo-video"></i>',
+                    'icon' => '<i class="fa fa-images"></i>',
                     // 'extras' => [PageMenuProvider::ORDER_NUMBER, 2],
                     'items' => [['route' => 'admin_app_media_list', 'label' => 'a']],
                 ],
