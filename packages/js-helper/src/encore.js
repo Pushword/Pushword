e8db53bab89c51feffdfa700fed4f0467d973e49
RobinDev <contact@piedweb.com>
2023-12-12 09:50:01 +0100

    update pushword/js-helper version

--- packages/js-helper/src/encore.js
+++ packages/js-helper/src/encore.js
@@ -5,14 +5,12 @@ const tailwindcss = require('tailwindcss');
 function getFilesToWatch(basePath = './..') {
     return [
         basePath + '/vendor/pushword/core/src/templates/**/*.html.twig',
-        basePath + '/vendor/pushword/core/src/templates/*.html.twig',
-        basePath + '/vendor/pushword/conversation/src/templates/*.html.twig',
-        basePath + '/vendor/pushword/admin-block-editor/src/templates/page/*.html.twig',
-        basePath + '/vendor/pushword/advanced-main-image/src/templates/page/*.html.twig',
-        basePath + '/templates/*.html.twig',
+        basePath + '/vendor/pushword/conversation/src/templates/**/*.html.twig',
+        basePath + '/vendor/pushword/admin-block-editor/src/templates/**/*.html.twig',
+        basePath + '/vendor/pushword/advanced-main-image/src/templates/**/*.html.twig',
         basePath + '/templates/**/*.html.twig',
-        basePath + '/templates/**/**/*.html.twig',
         basePath + '/var/TailwindGeneratorCache/*',
+        basePath + '/src/Twig/AppExtension.php',
     ];
 }
 function getTailwindConfig(watchFiles = null) {
