23a5cacb4996dea71be7e6372473223d9440e1ce
RobinDev <contact@piedweb.com>
2024-01-28 22:08:23 +0100

    disable html compressor (bis) until fixed

--- packages/static-generator/src/Generator/HtmlCompressor.php
+++ packages/static-generator/src/Generator/HtmlCompressor.php
@@ -10,6 +10,8 @@ class HtmlCompressor
     {
         // TODO wait for https://github.com/voku/simple_html_dom/pull/106 be merged to restor html compressor with PHP 8.3
         // and remove package https://github.com/devteam-emroc/simple_html_dom in base and core
-        return HtmlCompressorFactory::construct()->compress($html);
+        $html = preg_replace('/<!--(.*?)-->/s', '', $html);
+
+        return $html; // return HtmlCompressorFactory::construct()->compress($html);
     }
 }
