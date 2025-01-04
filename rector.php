7a1243d3d2c8f68b8dfa57f56056e65af8d9187a
RobinDev <contact@piedweb.com>
2025-01-04 21:12:52 +0100

    simplify rector conf

--- rector.php
+++ rector.php
@@ -3,11 +3,7 @@
 declare(strict_types=1);
 use Rector\CodeQuality\Rector\Identical\FlipTypeControlToUseExclusiveTypeRector;
 use Rector\Config\RectorConfig;
-use Rector\Php73\Rector\FuncCall\JsonThrowOnErrorRector;
 use Rector\Php81\Rector\FuncCall\NullToStrictStringFuncCallArgRector;
-use Rector\PHPUnit\CodeQuality\Rector\Class_\AddSeeTestAnnotationRector;
-use Rector\PHPUnit\CodeQuality\Rector\Class_\PreferPHPUnitThisCallRector;
-use Rector\Symfony\Set\SymfonySetList;
 
 $paths = [
     __DIR__.'/packages',
@@ -25,27 +21,12 @@ return RectorConfig::configure()
     ->withPaths($paths)
     ->withRootFiles()
     ->withSymfonyContainerPhp(__DIR__.'/packages/skeleton/tests/symfonyContainer.php')
-    ->withPhpSets()
     ->withPreparedSets(codeQuality: true, codingStyle: true, earlyReturn: true, typeDeclarations: true, instanceOf: true)
-    ->withSets([
-        SymfonySetList::SYMFONY_64,
-        SymfonySetList::SYMFONY_72,
-        SymfonySetList::SYMFONY_CODE_QUALITY,
-        SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION,
-        SymfonySetList::ANNOTATIONS_TO_ATTRIBUTES,
-    ])
     ->withAttributesSets(symfony: true, doctrine: true)
     ->withSkip([
         'packages/core/src/Twig/AppExtension.php',
         'packages/skeleton/src/Kernel.php',
         'packages/core/src/Component/App/AppConfig.php',
-        AddSeeTestAnnotationRector::class,
-        JsonThrowOnErrorRector::class,
-        // StrvalToTypeCastRector::class,
-        // IntvalToTypeCastRector::class,
-        PreferPHPUnitThisCallRector::class,
-        // OrderByKeyToClassConstRector::class, // inject deprecate Criteria::DESC instead of Order::Descending
-        // CallableThisArrayToAnonymousFunctionRector::class,
         NullToStrictStringFuncCallArgRector::class,
         FlipTypeControlToUseExclusiveTypeRector::class,
     ])
