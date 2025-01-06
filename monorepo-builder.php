b9adf1ea56852061c9389f4eca9a50705c6a3014
RobinDev <contact@piedweb.com>
2025-01-06 11:20:18 +0100

    update monorepo builder

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -1,6 +1,6 @@
 <?php
 
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\MonorepoBuilder\Config\MBConfig;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\AddTagToChangelogReleaseWorker;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\PushNextDevReleaseWorker;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\PushTagReleaseWorker;
@@ -9,27 +9,19 @@ use Symplify\MonorepoBuilder\Release\ReleaseWorker\SetNextMutualDependenciesRele
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\TagVersionReleaseWorker;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\UpdateBranchAliasReleaseWorker;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\UpdateReplaceReleaseWorker;
-use Symplify\MonorepoBuilder\ComposerJsonManipulator\ValueObject\ComposerJsonSection;
-use Symplify\MonorepoBuilder\Config\MBConfig;
-use Symplify\MonorepoBuilder\ValueObject\Option;
 
 return static function (MBConfig $mbConfig): void {
     $mbConfig->packageDirectories([
-        __DIR__ . '/packages',
+        __DIR__.'/packages',
     ]);
-
+    $mbConfig->workers([
+        UpdateReplaceReleaseWorker::class,
+        SetCurrentMutualDependenciesReleaseWorker::class,
+        AddTagToChangelogReleaseWorker::class,
+        TagVersionReleaseWorker::class,
+        PushTagReleaseWorker::class,
+        SetNextMutualDependenciesReleaseWorker::class,
+        UpdateBranchAliasReleaseWorker::class,
+        PushNextDevReleaseWorker::class,
+    ]);
 };
-
-// return static function (ContainerConfigurator $containerConfigurator): void {
-//     $services = $containerConfigurator->services();
-
-//     // release workers - in order to execute
-//     $services->set(UpdateReplaceReleaseWorker::class);
-//     $services->set(SetCurrentMutualDependenciesReleaseWorker::class);
-//     $services->set(AddTagToChangelogReleaseWorker::class);
-//     $services->set(TagVersionReleaseWorker::class);
-//     $services->set(PushTagReleaseWorker::class);
-//     $services->set(SetNextMutualDependenciesReleaseWorker::class);
-//     $services->set(UpdateBranchAliasReleaseWorker::class);
-//     $services->set(PushNextDevReleaseWorker::class);
-// };
