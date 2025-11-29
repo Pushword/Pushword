45859615a6c34b2901785ed9b9c24579ad6a8066
RobinDev <contact@piedweb.com>
2025-11-29 15:53:13 +0100

    ...

--- packages/admin/src/Controller/PageCrudController.php
+++ packages/admin/src/Controller/PageCrudController.php
@@ -32,7 +32,7 @@ use Pushword\Core\Utils\FlashBag;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Twig\Error\RuntimeError;
 use Twig\Error\SyntaxError;
 
