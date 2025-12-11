93ae28b4555000bbf3cfb369bf98dd594dd6e6bc
RobinDev <contact@robin-d.fr>
2025-12-11 14:46:29 +0100

    SimpleBackupCommand

---
+++ packages/core/src/Command/SimpleBackupCommand.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Pushword\Core\Command;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Attribute\Option;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Filesystem\Filesystem;
+
+#[AsCommand(name: 'pw:backup', description: 'Restore the last backup of the database.')]
+final class SimpleBackupCommand
+{
+    public function __construct(
+        private Filesystem $fs,
+    ) {
+    }
+
+    public function __invoke(
+        OutputInterface $output,
+        #[Option(name: 'create', shortcut: 'c')]
+        bool $create = false
+    ): int {
+        if ($create) {
+            $this->createBackup($output);
+
+            return Command::SUCCESS;
+        }
+
+        $this->restoreLastBackup($output);
+
+        return Command::SUCCESS;
+    }
+
+    public function createBackup(?OutputInterface $output = null): void
+    {
+        $backupFileName = 'var/app.db~'.date('YmdHis');
+        $this->fs->copy('var/app.db', $backupFileName);
+        $output?->writeln('Backup created: '.$backupFileName);
+    }
+
+    public function restoreLastBackup(?OutputInterface $output = null): void
+    {
+        $backupFiles = glob('var/app.db~*');
+        if ([] === $backupFiles || false === $backupFiles) {
+            $output?->writeln('<error>No backup files found</error>');
+
+            return;
+        }
+
+        sort($backupFiles);
+        $lastBackup = end($backupFiles);
+
+        $this->fs->copy($lastBackup, 'var/app.db', true);
+        $output?->writeln('Restored from: '.$lastBackup);
+    }
+}
