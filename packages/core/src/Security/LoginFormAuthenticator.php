d600659d881b6cfc9d8214f08ac9ad54d788af41
RobinDev <contact@piedweb.com>
2026-02-11 15:10:14 +0100

    Add RememberMeBadge to LoginFormAuthenticator
    Without this badge, the remember_me cookie was never set despite
    always_remember_me being enabled in security config. This caused
    users to be logged out whenever their PHP session was lost.

--- packages/core/src/Security/LoginFormAuthenticator.php
+++ packages/core/src/Security/LoginFormAuthenticator.php
@@ -9,6 +9,7 @@ use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\RememberMeBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
@@ -39,6 +40,7 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
             new PasswordCredentials((string) $request->request->get('password', '')),
             [
                 new CsrfTokenBadge('authenticate', (string) $request->request->get('_csrf_token')),
+                new RememberMeBadge(),
             ]
         );
     }
