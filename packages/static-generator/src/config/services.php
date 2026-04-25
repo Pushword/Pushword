5d8d1b34d9ba5bb7efa98bfbf3993843e062c219
RobinDev <contact@piedweb.com>
2026-04-25 14:32:18 +0200

    ...

--- packages/static-generator/src/config/services.php
+++ packages/static-generator/src/config/services.php
@@ -2,7 +2,10 @@
 
 use Pushword\Admin\PushwordAdminBundle;
 use Pushword\Core\PushwordCoreBundle;
+use Pushword\StaticGenerator\Cache\MessageHandler\PageCacheRefreshHandler;
+use Pushword\StaticGenerator\Cache\PageCacheInvalidator;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\Messenger\MessageBusInterface;
 
 use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
 
@@ -18,9 +21,21 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $adminInstalled = class_exists(PushwordAdminBundle::class);
     $adminExclude = $adminInstalled ? [] : [__DIR__.'/../Cache/Admin/'];
 
+    $messengerInstalled = interface_exists(MessageBusInterface::class);
+    $messengerExclude = $messengerInstalled ? [] : [
+        __DIR__.'/../Cache/MessageHandler/',
+        __DIR__.'/../Cache/PageCacheInvalidator.php',
+    ];
+
     $services->load('Pushword\StaticGenerator\\', __DIR__.'/../*')
         ->exclude([
             __DIR__.'/../'.PushwordCoreBundle::SERVICE_AUTOLOAD_EXCLUDE_PATH,
             ...$adminExclude,
+            ...$messengerExclude,
         ]);
+
+    if ($messengerInstalled) {
+        $services->set(PageCacheInvalidator::class);
+        $services->set(PageCacheRefreshHandler::class);
+    }
 };
